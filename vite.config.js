73cebec898d81d4d319c60898d538d6be33073be
Karekin <445923692@qq.com>
2025-05-25 00:30:05 +0800

    fix: 解决跨域问题

--- vite.config.js
+++ vite.config.js
@@ -29,6 +29,29 @@ export default (command, mode) => {
 			hmr: {
 				overlay: true,
 			},
+			proxy: {
+				'/app-api': {
+					target: 'http://127.0.0.1:48080',
+					changeOrigin: true,
+					secure: false,
+					configure: (proxy, _options) => {
+						proxy.on('error', (err, _req, _res) => {
+							console.log('proxy error', err);
+						});
+						proxy.on('proxyReq', (proxyReq, req, _res) => {
+							console.log('Sending Request to the Target:', req.method, req.url);
+						});
+						proxy.on('proxyRes', (proxyRes, req, _res) => {
+							console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+						});
+					},
+				},
+				'/admin-api': {
+					target: 'http://127.0.0.1:48080',
+					changeOrigin: true,
+					secure: false,
+				}
+			}
 		},
 	};
 };
